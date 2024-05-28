843e2ce8a51614ef93242baff089bc3ab1d2e8a4
Sanzhar <anapiyaevs0519@gmail.com>
2024-05-28 11:21:06 +0500

    merged_to_normloize infer datapackage

--- scripts/datapack-creation.py
+++ scripts/datapack-creation.py
@@ -2,6 +2,7 @@ from datapackage import Package
 
 
 package = Package()
+package.infer(r'archive/merged_to_normalize.csv')
 package.infer(r'data/final_rating.csv')
 package.commit()
 package.save('datapackage.json')
